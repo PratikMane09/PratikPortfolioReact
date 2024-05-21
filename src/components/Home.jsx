c63fb315d63011fbfe8f16348a978590b43eb18d
PratikMane09 <Pratik09092001@gmail.com>
2024-05-21 09:50:38 +0530

    Portfolio Done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -10,9 +10,7 @@ const Home = () => {
     <div name="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto  min-[300px]:pt-96 min-[320px]:pt-80  p-2 sm:pt-12 lg:pt-2 flex justify-center items-center">
         <div>
-          <p className="uppercase text-sm tracking-widest text-gray-700">
-            LET US SOLVE PROBLEMS TOGETHER
-          </p>
+          <p className="uppercase text-sm tracking-widest text-gray-700"></p>
           <h1 className="py-4 text-gray-800">
             Hi, I&#39;m <span className="text-[#5651e5]">PRATIK</span>
           </h1>
